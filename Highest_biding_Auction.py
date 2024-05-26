11b74142d0f5336528ff65c5eb1fb17dce80fffd
keerthi0619 <54984113+keerthi0619@users.noreply.github.com>
2024-05-26 15:23:32 -0500

    Add files via upload

---
+++ Highest_biding_Auction.py
@@ -0,0 +1,41 @@
+print('''
+                         ___________
+                         \         /
+                          )_______(
+                          |"""""""|_.-._,.---------.,_.-._
+                          |       | | |               | | ''-.
+                          |       |_| |_             _| |_..-'
+                          |_______| '-' `'---------'` '-'
+                          )"""""""(
+                         /_________\\
+                       .-------------.
+                      /_______________\\
+''')
+
+import subprocess
+subprocess.call(['clear']) 
+
+bids={}
+
+bidding_finished=False
+
+def find_highest_bidder(bidding_record):
+    highest_bid=0
+    winner=''
+    for bidder in bidding_record:
+        bid_amount=bidding_record[bidder]
+        if bid_amount>highest_bid:
+            highest_bid=bid_amount
+            winner=bidder
+    print(f"the winner is {winner} with the highest bid of {highest_bid}")
+
+while not bidding_finished:
+  name = input("What is your name?: ")
+  price = int(input("What is your bid?: $"))
+  bids[name] = price
+  should_continue = input("Are there any other bidders? Type 'yes or 'no'.\n")
+  if should_continue == "no":
+    bidding_finished = True
+    find_highest_bidder(bids)
+  elif should_continue == "yes":
+    subprocess.call(['clear']) 
