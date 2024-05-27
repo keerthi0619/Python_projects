31f5f3c56f68d724b26c0d8416ad81a1cf9d97d6
keerthi0619 <54984113+keerthi0619@users.noreply.github.com>
2024-05-26 22:09:03 -0500

    Add files via upload

---
+++ higher_lower_game.py
@@ -0,0 +1,90 @@
+#Generate random account from game data
+
+
+import random
+
+from subprocess
+
+from art import logo, vs
+from game_data import data
+
+
+def get_random_account():
+  """Get data from random account"""
+  return random.choice(data)
+
+#Format the acount data into printable format
+
+def format_data(account):
+  """Format account into printable format: name, description and country"""
+  name = account["name"]
+  description = account["description"]
+  country = account["country"]
+  # print(f'{name}: {account["follower_count"]}')
+  return f"{name}, a {description}, from {country}"
+
+
+
+#Ask user to guess
+
+def check_answer(guess, a_followers, b_followers):
+  """Checks followers against user's guess 
+  and returns True if they got it right.
+  Or False if they got it wrong.""" 
+  if a_followers > b_followers:
+    return guess == "a"
+  else:
+    return guess == "b"
+  
+#Check if user is correct
+
+# Get follower  count of each account
+
+def game():
+  print(logo)
+  score = 0
+  # Make the game repetable
+  game_should_continue = True
+  account_a = get_random_account()
+  account_b = get_random_account()
+
+  while game_should_continue:
+    account_a = account_b
+    account_b = get_random_account()
+
+    while account_a == account_b:
+      account_b = get_random_account()
+
+    print(f"Compare A: {format_data(account_a)}.")
+    print(vs)
+    print(f"Against B: {format_data(account_b)}.")
+    
+    guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+    a_follower_count = account_a["follower_count"]
+    b_follower_count = account_b["follower_count"]
+    is_correct = check_answer(guess, a_follower_count, b_follower_count)
+
+    clear()
+
+
+
+# use if stmt to check the correct answerr
+#Score keeping
+# Giver user feedback on thir guess
+    if is_correct:
+      score += 1
+      print(f"You're right! Current score: {score}.")
+    else:
+      game_should_continue = False
+      print(f"Sorry, that's wrong. Final score: {score}")
+
+game()
+
+
+
+
+
+
+# Making account at position b to be at position a 
+
+# clear the screens
